24fe91ccf11c3c9c3f54a5580fc95653118e1278
DhimazRafif <hanafidhimaz@gmail.com>
2026-04-05 16:24:30 +0700

    menambahkan class DataMahasiswa_103082430003

--- tpmodul7_kelompok_6/DataMahasiswa_103082430003.cs
+++ tpmodul7_kelompok_6/DataMahasiswa_103082430003.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace tpmodul7_kelompok_6
+{
+    public class DataMahasiswa
+    {
+        public string nama;
+        public string nim;
+        public string fakultas;
+    }
+    internal class DataMahasiswa_103082430003
+    {
+        public void ReadJSON()
+        {
+            string filename = "tp7_1_103082430003.json";
+            string json = File.ReadAllText(filename);
+
+            try
+            {
+                var options = new JsonSerializerOptions
+                {
+                    IncludeFields = true,
+                };
+
+                var mhs = JsonSerializer.Deserialize<DataMahasiswa>(json, options);
+
+                Console.WriteLine($"Nama {mhs.nama} dengan nim {mhs.nim} dari fakultas {mhs.fakultas}");
+            }
+            catch (JsonException ex)
+            {
+                Console.WriteLine("Error JSON parsing" + ex.Message );
+            }
+
+        }
+    }
+}
--- tpmodul7_kelompok_6/Program.cs
+++ tpmodul7_kelompok_6/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using tpmodul7_kelompok_6;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DataMahasiswa_103082430003 data = new DataMahasiswa_103082430003();
+        data.ReadJSON();
+    }
+}
